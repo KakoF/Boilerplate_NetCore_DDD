fb79e912921131870f8e21943f244c5a0868589c
Marcos <kakoferrare87@gmail.com>
2021-08-04 10:42:21 -0300

    change docker ports

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -8,18 +8,18 @@ namespace Api.Data.Context
     public MyContext CreateDbContext(string[] args)
     {
       var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
-      var connectionString = "Server=localhost;Port=5433;Database=SolidBase;User Id=kako;Password=kako123456;";
-      optionsBuilder.UseNpgsql(connectionString);
-      /*if (Environment.GetEnvironmentVariable("DATABASE").ToLower() == "MYSQL".ToLower())
+      //var connectionString = "Server=localhost;Port=5433;Database=SolidBase;User Id=kako;Password=kako123456;";
+      //optionsBuilder.UseNpgsql(connectionString);
+      if (Environment.GetEnvironmentVariable("DATABASE").ToLower() == "MYSQL".ToLower())
       {
-          optionsBuilder.UseMySql(Environment.GetEnvironmentVariable("MYSQL_DB_CONNECTION"));
-          //optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=SolidBase;Uid=kako;Pwd=kako123456;");
+        optionsBuilder.UseMySql(Environment.GetEnvironmentVariable("MYSQL_DB_CONNECTION"));
+        //optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=SolidBase;Uid=kako;Pwd=kako123456;");
       }
       else
       {
-          optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("POSTGRE_DB_CONNECTION"));
-          //optionsBuilder.UseNpgsql("Server=localhost;Port=5433;Database=SolidBase;User Id=kako;Password=kako123456;");
-      }*/
+        optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("POSTGRE_DB_CONNECTION"));
+        //optionsBuilder.UseNpgsql("Server=localhost;Port=5433;Database=SolidBase;User Id=kako;Password=kako123456;");
+      }
       return new MyContext(optionsBuilder.Options);
 
     }
